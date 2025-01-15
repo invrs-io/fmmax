3a38fb577abac64460014e67a8fd415cd1da182b
Martin Schubert <martinschubert@Mac.attlocal.net>
2025-01-14 21:29:17 -0800

    Version updated from v0.12.0 to v0.13.0

--- src/fmmax/__init__.py
+++ src/fmmax/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-__version__ = "v0.12.0"
+__version__ = "v0.13.0"
 
 from fmmax import _fft as _fft
 from fmmax import _fmm_matrices as _fmm_matrices
